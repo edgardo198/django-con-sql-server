564a767307b89fdc9c67d0cac4b15e676b0fcc76
edgardo198 <edgardo198@users.noreply.github.com>
2026-05-24 22:44:44 -0600

    Avoid resetting superadmin on deploy

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ set -o errexit
 mkdir -p "${DJANGO_MEDIA_ROOT:-media}"
 python manage.py migrate --noinput
 python manage.py media_diagnostics || true
-python manage.py bootstrap_access
+python manage.py bootstrap_access --if-empty
 python -m gunicorn app.wsgi:application --bind 0.0.0.0:${PORT:-8000}
